8d31b975ce350d81398f02066a804afaf7b8a35a
baninho <florianimbt@gmail.com>
2016-05-02 17:57:56 +0200

    View: Add BoardView, FieldView

---
+++ Unity/CircleWarsReloaded/Assets/BoardView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+    public static GameObject redToken;
+
+    // Use this for initialization
+    void Start () {
+        redToken = GameObject.Find("RedToken");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/CircleWarsReloaded/Assets/FieldView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Field : MonoBehaviour {
+
+    public int fieldID = -1;
+    
+
+    public void OnMouseDown()
+    {
+        Instantiate(Board.redToken);
+    }
+
+}
