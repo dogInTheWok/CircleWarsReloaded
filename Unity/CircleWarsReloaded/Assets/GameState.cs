360cdffa0c2d2cc7fdbec071e3b872e821673658
MattW <m.wehmer@gmail.com>
2016-05-03 16:09:34 +0200

    Engine: Add state handling.

--- Unity/CircleWarsReloaded/Assets/GameState.cs
+++ Unity/CircleWarsReloaded/Assets/GameState.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+
 namespace Engine
 {
+
     public class GameState
     {
         public enum State
@@ -9,7 +12,37 @@ namespace Engine
             RunningSecret,
             Eval
         }
+        
+        public abstract class Listener
+        {
+            public abstract void OnStateChange(State state);
+        }
 
-        public State Value { get; set; }
+        public GameState()
+        {
+            listeners = new List<Listener>();
+        }
+        public State Value
+        {
+            get { return value; }
+            set
+            {
+                if (value == this.value)
+                    return;
+                this.value = value;
+                foreach( Listener listener in listeners )
+                {
+                    listener.OnStateChange(value);
+                }
+            }
+        }
+
+        public void RegisterOnStateChange(Listener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        private State value;
+        private List<Listener> listeners;
     }
 }
--- Unity/CircleWarsReloaded/Assets/StatusTextView.cs
+++ Unity/CircleWarsReloaded/Assets/StatusTextView.cs
@@ -3,24 +3,34 @@ using UnityEngine.UI;
 using System.Collections;
 using Engine;
 
-public class StatusTextView : MonoBehaviour {
+public class StatusTextView: MonoBehaviour {
+
+    class StateListener : GameState.Listener
+    {
+        public StateListener( ref Text parent)
+        {
+
+        }
+        public override void OnStateChange(GameState.State state )
+        {
+            text.text = state.ToString();
+        }
+        private Text text;
+    }
 
     [SerializeField] private GameView gameView;
-    private Text text;
+    private StateListener stateListener;
+    
 
     // Use this for initialization
 	void Start () {
-        text = GetComponent<Text>();
-        text.text = gameView.Game.CurrentState.Value.ToString();
+        var text = GetComponent<Text>();
+        stateListener = new StateListener(text);
+        gameView.Game.CurrentState.RegisterOnStateChange(stateListener);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
-    public void OnClickStartButton()
-    {
-        text.text = gameView.Game.CurrentState.Value.ToString();
-    }
 }
