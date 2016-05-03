9b988adf5256d4a4760e5d75104e15e1ec78e441
MattW <m.wehmer@gmail.com>
2016-05-03 16:56:43 +0200

    View: Add stateListener to FieldView.

--- Unity/CircleWarsReloaded/Assets/FieldView.cs
+++ Unity/CircleWarsReloaded/Assets/FieldView.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Engine;
+using System;
 
 public class FieldView : MonoBehaviour {
+    public class StateListener : GameState.Listener
+    {
+        public StateListener( FieldView parent)
+        {
+            this.parent = parent;
+        }
+        public override void OnStateChange(GameState.State state)
+        {
+            if( GameState.State.Eval == state )
+            {
+                //TODO
+                parent.AddThreeVisualTokens();
+            }
+        }
+        private FieldView parent;
+    } 
 
     [SerializeField] private BoardView boardView;
     [SerializeField] private FieldView[] neighbours;
+    private StateListener stateListener;
 
     public Field Field { get; private set; }
 
     public void Start()
     {
         Field = Game.Instance().createField();
-
+        stateListener = new StateListener( this );
+        Game.Instance().CurrentState.RegisterOnStateChange(stateListener);
         foreach( FieldView view in neighbours)
         {
             Field.addNeighbour(view.Field);
@@ -38,5 +57,10 @@ public class FieldView : MonoBehaviour {
             boardView.addPlayer1Token();
         }
 
+    }
+
+    public void AddThreeVisualTokens()
+    {
+        Debug.Log("Test Add three tokens");
     }
 }
