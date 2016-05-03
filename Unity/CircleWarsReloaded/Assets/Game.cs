b8b8a94541b97a0b5e64ce78910d07d4402895b0
MattW <m.wehmer@gmail.com>
2016-05-03 12:25:29 +0200

    Engine: Add Game.State.

--- Unity/CircleWarsReloaded/Assets/Game.cs
+++ Unity/CircleWarsReloaded/Assets/Game.cs
@@ -4,98 +4,121 @@
 using UnityEngine;
 using System.Collections;
 
-namespace Engine {
-
-public class Game {
-	static public int NUM_FIELDS = 12;
-    static public int NUM_PLAYER = 2;
-	static public int NUM_FORCES_DISTRIB_PHASE = 14;
-	static public int NUM_TURNS_DISTRIB = NUM_PLAYER * NUM_FORCES_DISTRIB_PHASE;
-
-    static public Game Instance() {
-            if( null == s_instance )
+namespace Engine
+{
+    public class GameSingleton
+    {
+        static public Game Instance()
+        {
+            if (null == s_instance)
             {
-                s_instance = new Game( new GlobalFactory() );
-                s_instance.StartGame();
+                s_instance = new Game(new GlobalFactory());
             }
 
             return s_instance;
+        }
+        static private Game s_instance;
     }
 
-    static private Game s_instance;
+    public class Game : GameSingleton
+    {
+        public enum State
+        {
+            NotStarted,
+            Running,
+            Aborted,
+            Won
+        }
 
-    public bool isStarted { get; private set; }
-	private int distribTurn;
+        static public int NUM_FIELDS = 12;
+        static public int NUM_PLAYER = 2;
+        static public int NUM_FORCES_DISTRIB_PHASE = 14;
+        static public int NUM_TURNS_DISTRIB = NUM_PLAYER * NUM_FORCES_DISTRIB_PHASE;
 
-    private PlayerList playerList;
-	private FieldList fieldList;
-    
-    public Field createField() {
+        public State CurrentState { get; private set;}
+        public bool isStarted { get; private set; }
+        private int distribTurn;
+
+        private PlayerList playerList;
+        private FieldList fieldList;
+
+        public Field createField()
+        {
             return fieldList.createField();
-    }
+        }
 
-    public Game(GlobalFactory factory){
-        playerList = factory.createPlayerList();
-	    fieldList = factory.createFieldList();
-        isStarted = false;
-    }
+        public Game(GlobalFactory factory)
+        {
+            playerList = factory.createPlayerList();
+            fieldList = factory.createFieldList();
+            isStarted = false;
+        }
 
-    public void StartGame() {
-	    init();
-	    playerList.StartGame();
-	    isStarted = true;
-    }
+        public void StartGame()
+        {
+            init();
+            playerList.StartGame();
+            isStarted = true;
+        }
 
-	private void init() {
-		fillPlayerList();
-		distribTurn = 0;
-	}
+        private void init()
+        {
+            fillPlayerList();
+            distribTurn = 0;
+        }
 
-	private void fillPlayerList() {
-		playerList.Add(new Player(Player.ID.PLAYER1));
-		playerList.Add(new Player(Player.ID.PLAYER2));
-	}
+        private void fillPlayerList()
+        {
+            playerList.Add(new Player(Player.ID.PLAYER1));
+            playerList.Add(new Player(Player.ID.PLAYER2));
+        }
 
-	public void AddPlayer(Player player){
-        playerList.Add(player);
-    }
+        public void AddPlayer(Player player)
+        {
+            playerList.Add(player);
+        }
 
-    public int NumPlayer() {
-        return playerList.Size();
-    }
+        public int NumPlayer()
+        {
+            return playerList.Size();
+        }
 
-	public int NumFields() {
-		return fieldList.size();
-	}
+        public int NumFields()
+        {
+            return fieldList.size();
+        }
 
-	public Player.ID ActivePlayer() {
-		return playerList.ActivePlayer();
-	}
+        public Player.ID ActivePlayer()
+        {
+            return playerList.ActivePlayer();
+        }
 
-	public void NextTurn() {
+        public void NextTurn()
+        {
             if (distribTurn == NUM_TURNS_DISTRIB)
             {
                 EnterSecretPhase();
                 return;
             }
-		playerList.NextPlayer();
-		distribTurn = distribTurn++;
-	}
+            playerList.NextPlayer();
+            distribTurn = distribTurn++;
+        }
 
-	public bool DispatchForce(Field field) {
+        public bool DispatchForce(Field field)
+        {
             if (field.addToken())
             {
                 NextTurn();
                 return true;
             }
             return false;
-	}
+        }
 
-    public void EnterSecretPhase()
+        public void EnterSecretPhase()
         {
             // TODO: state machine phases
             return;
         }
-}
+    }
 
 } //Namespace Engine
