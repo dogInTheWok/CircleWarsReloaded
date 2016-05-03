f7d3dfc8919f92a5970a68609d14791979ca394a
MattW <m.wehmer@gmail.com>
2016-05-03 10:54:32 +0200

    Unity: Update project settings.

---
+++ Unity/CircleWarsReloaded/Assets/GameView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Engine;
+
+public class GameView : MonoBehaviour {
+    private Game game;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
