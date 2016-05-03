f7ca5ec4e8126f0acfd05b8a3e624158993dda64
MattW <m.wehmer@gmail.com>
2016-05-03 12:02:50 +0200

    GUI: Add StatusVIew and manipulation.

---
+++ Unity/CircleWarsReloaded/Assets/StatusTextView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Engine;
+
+public class StatusTextView : MonoBehaviour {
+
+    [SerializeField] private GameView gameView;
+
+    // Use this for initialization
+	void Start () {
+        var game = gameView.Game;
+        var text = GetComponent<Text>();
+        text.text = "Started";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
