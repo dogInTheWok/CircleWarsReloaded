7330d37be28337ef06a5c5b5c44a6b3caf35030b
MattW <m.wehmer@gmail.com>
2016-05-06 11:06:07 +0200

    View: Fix remarks StatusText.

--- Unity/CircleWarsReloaded/Assets/StatusTextView.cs
+++ Unity/CircleWarsReloaded/Assets/StatusTextView.cs
@@ -5,31 +5,25 @@ using Engine;
 
 public class StatusTextView : MonoBehaviour
 {
-    [SerializeField]
-    private GameView gameView;
-
     private Text text;
 
     // Use this for initialization
     void Start()
     {
         text = GetComponent<Text>();
-        gameView.Game.CurrentGameState.ConnectTo(OnStateChange);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        Game.Instance().CurrentGameState.ConnectTo(OnStateChange);
     }
 
     // Implement slot on GameStateChange
     public void OnStateChange(Game.GameState state)
     {
-        text.text = state.ToString();
         if (state == Game.GameState.Terminated)
         {
             text.text = Game.Instance().winner.ToString() + " wins!";
+        }
+        else
+        {
+            text.text = state.ToString();
         }
     }
 }
