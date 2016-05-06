4047af7f9b2b911222c08d06fc1c1d8beafd225c
MattW <m.wehmer@gmail.com>
2016-05-06 11:02:28 +0200

    View: Fix remarks on StartButtonTextView.

--- Unity/CircleWarsReloaded/Assets/StartButtonTextView.cs
+++ Unity/CircleWarsReloaded/Assets/StartButtonTextView.cs
@@ -5,27 +5,20 @@ using UnityEngine.UI;
 
 public class StartButtonTextView : MonoBehaviour
 {
-    private Text text;        
+    private Text text;
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         text = GetComponent<Text>();
         Game.Instance().CurrentGameState.ConnectTo(OnStateChange);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
     // Implement slot on GameState
     public void OnStateChange(Game.GameState state)
     {
         switch (state)
         {
             case Game.GameState.NotStarted:
-                text.text = "Start";
-                break;
-            case Game.GameState.Terminated:
                 text.text = "Start";
                 break;
             default:
