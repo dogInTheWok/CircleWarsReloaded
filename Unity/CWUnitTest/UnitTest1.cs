ff659e6f6a1789b74a711ff4446f4b2f990b1454
baninho <florianimbt@gmail.com>
2016-12-10 14:21:14 +0100

    Add Unit Test

---
+++ Unity/CWUnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Engine;
+
+namespace CWUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGame()
+        {
+            Game game = new Game();
+            Assert.AreEqual(Game.GameState.NotStarted, game.CurrentGameState.Value);
+        }
+
+        [TestMethod]
+        public void TestStartGame()
+        {
+            Game game = new Game();
+            game.Start();
+            Assert.AreEqual(Game.GameState.RunningDistribution, game.CurrentGameState.Value);
+        }
+    }
+}
